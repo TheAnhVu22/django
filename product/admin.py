966de0058562434c0dbf754e577e804bfcfac347
Vu The Anh <theanhvu06@gmail.com>
2022-04-15 18:52:52 +0700

    update

--- product/admin.py
+++ product/admin.py
@@ -1,13 +1,77 @@
 from django.contrib import admin
-from .models import product, category
-class ProductAdmin(admin.ModelAdmin):
-	list_display = ('ten','gia','mota','anh','cate_id')
-	search_fields = ['ten']
-	list_filter = ('ten','gia','mota','cate_id')
-admin.site.register(product,ProductAdmin)
+from .models import  category, brand,product,gallery,rating,comment,customer,order,shipping,order_detail,post,cate_post
 
 class CategoryAdmin(admin.ModelAdmin):
-	list_display = ('ten','mota')
-	search_fields = ['ten']
-	list_filter = ('ten','mota')
+	list_display = ('cate_name','cate_desc')
+	search_fields = ['cate_name']
+	list_filter = ('cate_name','cate_desc')
 admin.site.register(category,CategoryAdmin)
+
+class BrandAdmin(admin.ModelAdmin):
+	list_display = ('brand_name','brand_desc')
+	search_fields = ['brand_name']
+	list_filter = ('brand_name','brand_desc')
+admin.site.register(brand,BrandAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ('pro_name','pro_price','pro_quantity','pro_desc','pro_image')
+	search_fields = ['pro_name']
+	list_filter = ('pro_name','pro_price','pro_desc','category_id','brand_id')
+admin.site.register(product,ProductAdmin)
+
+
+class GalleryAdmin(admin.ModelAdmin):
+	list_display = ('product_id','gallery_image','gallery_name')
+	search_fields = ['gallery_name']
+	list_filter = ('product_id','gallery_name')
+admin.site.register(gallery,GalleryAdmin)
+
+class RatingAdmin(admin.ModelAdmin):
+	list_display = ('product_id','rating')
+	search_fields = ['product_id']
+	list_filter = ('product_id','rating')
+admin.site.register(rating,RatingAdmin)
+
+class CommentAdmin(admin.ModelAdmin):
+	list_display = ('comment_name','comment_content','product_id','comment_parent_id')
+	search_fields = ['comment_name']
+	list_filter = ('comment_name','comment_content','product_id')
+admin.site.register(comment,CommentAdmin)
+
+class CustomerAdmin(admin.ModelAdmin):
+	list_display = ('cus_name','cus_email','cus_phone')
+	search_fields = ['cus_name']
+	list_filter = ('cus_name','cus_email','cus_phone')
+admin.site.register(customer,CustomerAdmin)
+
+class ShippingAdmin(admin.ModelAdmin):
+	list_display = ('ship_name','ship_email','ship_phone','ship_address','ship_note','ship_method')
+	search_fields = ['ship_name','ship_email']
+	list_filter = ('ship_name','ship_email','ship_phone','ship_method')
+admin.site.register(shipping,ShippingAdmin)
+
+class OrderAdmin(admin.ModelAdmin):
+	list_display = ('cus_id','shipping_id','order_code','order_status')
+	search_fields = ['order_code']
+	list_filter = ('order_code','order_status')
+admin.site.register(order,OrderAdmin)
+
+
+
+class OrderDetailAdmin(admin.ModelAdmin):
+	list_display = ('order_code','product_id','product_name','product_price','product_sale_quantity')
+	search_fields = ['order_code']
+	list_filter = ('order_code','product_id','product_name','product_price')
+admin.site.register(order_detail,OrderDetailAdmin)
+
+class CatePostAdmin(admin.ModelAdmin):
+	list_display = ('cate_post_name','cate_post_status')
+	search_fields = ['cate_post_name']
+	list_filter = ('cate_post_name','cate_post_status')
+admin.site.register(cate_post,CatePostAdmin)
+
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('post_name','post_content','post_image','post_author','cate_post_id')
+	search_fields = ['post_name','post_author']
+	list_filter = ('post_name','post_author')
+admin.site.register(post,PostAdmin)
--- product/models.py
+++ product/models.py
@@ -1,17 +1,101 @@
 from django.db import models
 
+
+
 # Create your models here.
-class product(models.Model):
-    ten = models.CharField(max_length=30)
-    gia = models.CharField(max_length=30)
-    mota = models.CharField(max_length=30)
-    anh = models.ImageField(upload_to="images", null=False, default=None)
-    cate_id = models.IntegerField(null=False, default=None)
-    def __str__(self):
-    	return f"{self.ten},{self.gia},{self.mota},{self.anh},{self.cate_id}"
 
 class category(models.Model):
-    ten = models.CharField(max_length=30)
-    mota = models.CharField(max_length=255)
+    cate_name = models.CharField(max_length=255, default=None)
+    cate_desc = models.CharField(max_length=255,default=None)
     def __str__(self):
-        return f"{self.ten},{self.mota}"    
+        return f"{self.cate_name},{self.cate_desc}"    
+
+class brand(models.Model):
+    brand_name = models.CharField(max_length=255, default=None)
+    brand_desc = models.CharField(max_length=255,default=None)
+    def __str__(self):
+        return f"{self.brand_name},{self.brand_desc}" 
+
+class product(models.Model):
+    pro_name = models.CharField(max_length=255,default=None)
+    pro_price = models.IntegerField(default=None)
+    pro_quantity = models.IntegerField(default=None)
+    pro_desc = models.CharField(max_length=255,default=None)
+    pro_image = models.ImageField(upload_to="images", null=False, default=None)
+    category_id = models.ForeignKey(category, on_delete=models.CASCADE)
+    brand_id = models.ForeignKey(brand, on_delete=models.CASCADE)
+    def __str__(self):
+    	return f"{self.pro_name},{self.pro_price},{self.pro_quantity},{self.pro_desc},{self.pro_image}"
+
+class gallery(models.Model):
+    product_id = models.ForeignKey(product, on_delete=models.CASCADE)
+    gallery_image =models.ImageField(upload_to="images", null=False, default=None)
+    gallery_name = models.CharField(max_length=255)
+    def __str__(self):
+        return f"{self.gallery_image},{self.gallery_name}" 
+
+class rating(models.Model):
+    product_id = models.ForeignKey(product, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    def __str__(self):
+        return f"{self.rating}" 
+
+class comment(models.Model):
+    comment_name = models.CharField(max_length=255)
+    comment_content = models.CharField(max_length=255)
+    product_id = models.ForeignKey(product, on_delete=models.CASCADE)
+    comment_parent_id = models.IntegerField(null=False, default=None)
+    def __str__(self):
+        return f"{self.comment_name},{self.comment_content},{self.comment_parent}" 
+
+class customer(models.Model):
+    cus_name = models.CharField(max_length=255)
+    cus_email = models.CharField(max_length=255)
+    cus_phone = models.CharField(max_length=30)
+    cus_password = models.CharField(max_length=50)
+    def __str__(self):
+        return f"{self.cus_name},{self.cus_email},{self.cus_phone},{self.cus_password}" 
+
+class shipping(models.Model):
+    ship_name = models.CharField(max_length=255)
+    ship_email = models.CharField(max_length=255)
+    ship_phone = models.CharField(max_length=30)
+    ship_address = models.CharField(max_length=30)
+    ship_note=models.CharField(max_length=255)
+    ship_method=models.CharField(max_length=30)
+    def __str__(self):
+        return f"{self.ship_name},{self.ship_email},{self.ship_phone},{self.ship_address},{self.ship_note},{self.ship_method}" 
+
+class order(models.Model):
+    cus_id = models.IntegerField(null=False, default=None)
+    shipping_id = models.IntegerField(null=False, default=None)
+    order_code = models.CharField(max_length=50)
+    order_status = models.CharField(max_length=255)
+    def __str__(self):
+        return f"{self.order_code},{self.cus_id},{self.shipping_id},{self.order_status}" 
+
+
+class order_detail(models.Model):
+    order_code = models.CharField(max_length=50)
+    product_id = models.IntegerField(null=False, default=None)
+    product_name = models.CharField(max_length=255)
+    product_price = models.IntegerField()
+    product_sale_quantity = models.IntegerField()
+    def __str__(self):
+        return f"{self.order_code},{self.product_id},{self.product_name},{self.product_price},{self.product_sale_quantity}" 
+
+class cate_post(models.Model):
+    cate_post_name = models.CharField(max_length=255)
+    cate_post_status = models.CharField(max_length=255)
+    def __str__(self):
+        return f"{self.cate_post_name},{self.cate_post_status}" 
+
+class post(models.Model):
+    post_name = models.CharField(max_length=255)
+    post_content= models.CharField(max_length=255)
+    post_image = models.ImageField(upload_to="images", null=False, default=None)
+    post_author = models.CharField(max_length=255)
+    cate_post_id = models.ForeignKey(cate_post, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.post_name},{self.post_content},{self.post_image},{self.post_author}" 
+
